448e1f33f8cfe74fff9f487c39becfd69a2b0703
Caleb Jamison <caleb@cbjamo.com>
2026-06-06 00:28:51 -0400

    testing

--- source/postcard-rpc/src/server/impls/mod.rs
+++ source/postcard-rpc/src/server/impls/mod.rs
@@ -63,7 +63,9 @@ pub(crate) mod embassy_shared {
         Sp: WireSpawn<Error = SpawnError, Info = Spawner>,
     {
         let info = sp.info();
-        info.spawn(tok)
+        info.spawn(tok);
+
+        Ok(())
     }
 }
 
